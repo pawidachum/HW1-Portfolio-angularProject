9c39d57943a5f9931222295fdf0c930115141582
pawidachum <pawida.may@gmail.com>
2021-03-15 15:47:23 +0700

    finish

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,47 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { MyEducationComponent } from './my-education/my-education.component';
+import { MyExperienceComponent } from './my-experience/my-experience.component';
+import { MySkillComponent } from './my-skill/my-skill.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'my-education',
+    component: MyEducationComponent
+  },
+  {
+    path: 'my-experience',
+    component: MyExperienceComponent
+  },
+  {
+    path: 'my-profile',
+    component: MyProfileComponent
+  },
+  {
+    path: 'my-skill',
+    component: MySkillComponent
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MyProfileComponent,
+    MyEducationComponent,
+    MyExperienceComponent,
+    MySkillComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
